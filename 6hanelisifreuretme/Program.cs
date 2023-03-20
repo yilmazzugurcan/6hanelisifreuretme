8ec1a3704d92dd1d6419a7f3a4f6dc79eba4c0c9
yilma <yilma@DESKTOP-U1LH654>
2023-03-20 15:27:27 +0300

    Add project files.

---
+++ 6hanelisifreuretme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6hanelisifreuretme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] sayilar = new int[6];
+
+            
+            Random rastgele = new Random();
+
+            
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rastgele.Next(10); 
+            }
+
+            
+            Console.WriteLine("Rastgele Sayılar:");
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write(sayilar[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
